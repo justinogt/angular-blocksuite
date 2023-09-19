269be0f9d0fdf2347619f3aeac89ffa64eb84761
Jorge Justino <jorge.justino@mobilibus.com.br>
2023-09-19 08:58:58 -0300

    Add blocksuite

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { SimpleAffineEditor } from '@blocksuite/editor';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-blocksuite';
+
+  ngOnInit(): void {
+    const editor = new SimpleAffineEditor();
+    document.body.appendChild(editor);
+  }
 }
