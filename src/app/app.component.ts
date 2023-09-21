31eb0d68d0c43cfa8fa971957e8f20f6add3b103
fourdim <59462000+fourdim@users.noreply.github.com>
2023-09-21 09:59:24 +0800

    fix: make it work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { SimpleAffineEditor } from '@blocksuite/editor';
 
+console.assert(SimpleAffineEditor !== undefined);
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'angular-blocksuite';
-
-  ngOnInit(): void {
-    const editor = new SimpleAffineEditor();
-    document.body.appendChild(editor);
-  }
 }
+
